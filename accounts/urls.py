26ee5591fc68e68911727a6abcfd14073e01ad32
Han <dhan@activenetwork.com>
2018-09-10 15:12:13 -0700

    transaction type,location,cards setup

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,21 @@ urlpatterns = [
     path('logout',views.logout, name='logout'),
     path('setup',views.setup,name='setup'),
     path('myaccount',views.myaccount,name='myaccount'),
+
+    path('paymentmethodsetup_page',views.paymentmethodsetup_page,name='paymentmethodsetup_page'),
     path('paymentmethodsetup',views.paymentmethodsetup,name='paymentmethodsetup'),
+    path('cards/<int:pm_id>',views.paymentmethodmodify_page,name='paymentmethodmodify_page'),
+    path('paymentmethodmodify',views.paymentmethodmodify,name='paymentmethodmodify'),
+
+    path('location_setup_page',views.location_setup_page,name='location_setup_page'),
+    path('location_setup',views.location_setup,name='location_setup'),
+    path('locations/<int:location_id>',views.location_modify_page,name='location_modify_page'),
+    path('location_modify',views.location_modify,name='location_modify'),
+
+    path('transaction_type_setup_page',views.transaction_type_setup_page,name='transaction_type_setup_page'),
+    path('transaction_type_setup',views.transaction_type_setup,name='transaction_type_setup'),
+    path('transaction-type/<int:type_id>',views.transaction_type_modify_page,name='transaction_type_modify_page'),
+    path('transaction_type_modify',views.transaction_type_modify,name='transaction_type_modify'),
+
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
@@ -44,10 +44,15 @@ def home(request):
     return render(request,'accounts/home.html')
 def setup(request):
     payment_methods = PaymentMethod.objects
-    return render(request,'accounts/setup.html',{'payment_methods':payment_methods})
+    locations = Location.objects
+    types = TransactionType.objects
+    return render(request,'accounts/setup.html',{'payment_methods':payment_methods,'locations':locations,"types":types})
 def myaccount(request):
     return render(request,'accounts/myaccount.html')
 
+@login_required
+def paymentmethodsetup_page(request):
+    return render(request,'accounts/card_setup.html')
 @login_required
 def paymentmethodsetup(request):
     logger = logging.getLogger(__name__)
@@ -60,7 +65,6 @@ def paymentmethodsetup(request):
         logger.error(timezone.datetime.now())
         if request.POST['description'] and request.POST['credit_limit'] and request.POST['status']:
             try:
-
                 payment_method = PaymentMethod()
                 payment_method.method_name=request.POST['description']
                 if request.POST['status'] == 'Active':
@@ -83,3 +87,199 @@ def paymentmethodsetup(request):
             return render(request, 'accounts/signup.html', {'error': 'password must match'})
     else:
         return render(request, 'accounts/signup.html')
+
+@login_required
+def paymentmethodmodify_page(request,pm_id):
+    record = get_object_or_404(PaymentMethod,pk=pm_id)
+    return render(request, 'accounts/card_modify.html',{'payment_method':record})
+@login_required
+def paymentmethodmodify(request):
+    logger = logging.getLogger(__name__)
+    logger.error("it is in")
+    if request.method == 'POST':
+        logger.error("it is post")
+        if request.POST['description'] and request.POST['credit_limit'] and request.POST['status']:
+            logger.error(request.POST['id'])
+            logger.error(request.POST['description'])
+            logger.error(request.POST['credit_limit'])
+            logger.error(request.POST['status'])
+            try:
+
+                payment_method = get_object_or_404(PaymentMethod,pk=request.POST['id'])
+                if payment_method:
+                    payment_method.method_name=request.POST['description']
+                    if request.POST['status'] == 'Active':
+                        payment_method.status = 1
+                    else:
+                        payment_method.status = 0
+
+                    payment_method.credit_limit=request.POST['credit_limit']
+
+                    payment_method.account_id = request.user.id
+
+                    payment_method.create_date = timezone.now()
+
+                    payment_method.save()
+                    return redirect('setup')
+            except:
+                    return render(request, 'setup.html', {'error': 'error occurred while saving records'})
+
+@login_required
+def locationModify_page(request):
+    pass
+@login_required
+def location_setup(request):
+    logger = logging.getLogger(__name__)
+    logger.error("location setup view function")
+    if request.method == 'POST':
+        logger.error(request.user.id)
+        if request.POST['description'] and request.POST['status']:
+            try:
+                location = Location()
+                location.name = request.POST['description']
+                if request.POST['street']:
+                    location.street = request.POST['street']
+                    logger.error(request.POST['street'])
+                else:
+                    location.street = ''
+                if request.POST['city']:
+                    location.city = request.POST['city']
+                    logger.error(request.POST['city'])
+                else:
+                    location.city = ''
+                if request.POST['province']:
+                    location.province = request.POST['province']
+                    logger.error(request.POST['province'])
+                else:
+                    location.province = ''
+                if request.POST['country']:
+                    location.country = request.POST['country']
+                    logger.error(request.POST['country'])
+                else:
+                    location.country = ''
+                if request.POST['postal_code']:
+                    location.postal_code = request.POST['postal_code']
+                    logger.error(request.POST['postal_code'])
+                else:
+                    location.postal_code = ''
+                if request.POST['status'] == 'Active':
+                    logger.error(request.POST['status'])
+                    location.status = 1
+                else:
+                    location.status = 0
+                location.account_id = request.user.id
+                location.save()
+                return redirect('setup')
+            except:
+                return render(request, 'setup.html', {'error': 'Please check your inputs and try again.'})
+@login_required
+def location_setup_page(request):
+    return render(request, 'accounts/location_setup.html')
+@login_required
+def location_modify_page(request,location_id):
+    record = get_object_or_404(Location, pk=location_id)
+    return render(request,'accounts/location-modify.html',{'location':record})
+@login_required
+def location_modify(request):
+    if request.method == 'POST':
+        try:
+            if request.POST['description'] and request.POST['status']:
+                location = get_object_or_404(Location,pk=request.POST['id'])
+                location.name = request.POST['description']
+                if request.POST['street']:
+                    location.street = request.POST['street']
+                    # logger.error(request.POST['street'])
+                else:
+                    location.street = ''
+                if request.POST['city']:
+                    location.city = request.POST['city']
+                    # logger.error(request.POST['city'])
+                else:
+                    location.city = ''
+                if request.POST['province']:
+                    location.province = request.POST['province']
+                    # logger.error(request.POST['province'])
+                else:
+                    location.province = ''
+                if request.POST['country']:
+                    location.country = request.POST['country']
+                    # logger.error(request.POST['country'])
+                else:
+                    location.country = ''
+                if request.POST['postal_code']:
+                    location.postal_code = request.POST['postal_code']
+                    # logger.error(request.POST['postal_code'])
+                else:
+                    location.postal_code = ''
+                if request.POST['status'] == 'Active':
+                    # logger.error(request.POST['status'])
+                    location.status = 1
+                else:
+                    location.status = 0
+                # location.account_id = request.user.id
+                location.save()
+                return redirect('setup')
+            else:
+                return render(request, 'setup.html', {'error': 'Please check your inputs and try again.'})
+        except:
+            return render(request, 'setup.html', {'error': 'Please check your inputs and try again.'})
+
+
+@login_required
+def transaction_type_setup_page(request):
+    return render(request, 'accounts/type-setup.html')
+
+@login_required
+def transaction_type_setup(request):
+    if request.method == 'POST':
+        logger = logging.getLogger(__name__)
+        logger.error(request.POST['income_indicator'])
+        if request.POST['description'] and request.POST['income_indicator'] and request.POST['status']:
+            type = TransactionType()
+            type.name = request.POST['description']
+            if request.POST['income_indicator'] == 'Income':
+                type.income_ind = 2
+            elif request.POST['income_indicator'] == 'Expense':
+                type.income_ind = 1
+            if request.POST['status'] == 'Active':
+                type.status = 1
+            else:
+                type.status = 0
+            type.account_id = request.user.id
+            type.save()
+            return redirect('setup')
+        else:
+            return render(request, 'setup.html', {'error': 'Please check your inputs and try again.'})
+
+@login_required
+def transaction_type_modify_page(request,type_id):
+    record = get_object_or_404(TransactionType, pk=type_id)
+    return render(request, 'accounts/transaction-type-modify.html', {'type': record})
+
+@login_required
+def transaction_type_modify(request):
+    logger = logging.getLogger(__name__)
+    if request.method == 'POST':
+        logger.error("entered")
+        try:
+
+            logger.error(request.POST['income_indicator'])
+            if request.POST['description'] and request.POST['income_indicator'] and request.POST['status']:
+
+                type = get_object_or_404(TransactionType,pk=request.POST['id'])
+                type.name = request.POST['description']
+                logger.error(request.POST['description'])
+                if request.POST['income_indicator'] == 'Income':
+                    type.income_ind = 2
+                elif request.POST['income_indicator'] == 'Expense':
+                    type.income_ind = 1
+                if request.POST['status'] == 'Active':
+                    type.status = 1
+                else:
+                    type.status = 0
+                type.save()
+                return redirect('setup')
+            else:
+                return render(request, 'setup.html', {'error': 'Please check your inputs and try again.'})
+        except:
+            return render(request, 'setup.html', {'error': 'Please check your inputs and try again.'})
